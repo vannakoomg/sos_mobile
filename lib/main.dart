29a95852dec2c6a1e0b2e29b8ace4cd8bf941cf7
Vannak1111 <vannak251020@gmail.com>
2023-04-19 16:08:30 +0700

    submit notification android

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,76 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:global_configuration/global_configuration.dart';
 import 'package:sos_mobile/utils/helpers/local_data/storge_local.dart';
 import 'configs/route/route.dart';
 import 'configs/theme/theme.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.notification!.title}");
+}
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_cannel', 'high Importance Notification',
+    importance: Importance.high, playSound: true);
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+ FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  if (Platform.isIOS) {
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true, // Required to display a heads up notification
+      badge: true,
+      sound: true,
+    );
+  }
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+      String? token = await FirebaseMessaging.instance.getToken();
+    debugPrint(" your get token $token");
+  if (messaging.isAutoInitEnabled) {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      debugPrint("you have been get notification from firebase ");
+      RemoteNotification? notification = message.notification;
+      if (message.notification != null) {
+      
+        if (Platform.isAndroid) {
+          await flutterLocalNotificationsPlugin
+              .resolvePlatformSpecificImplementation<
+                  AndroidFlutterLocalNotificationsPlugin>()
+              ?.createNotificationChannel(channel);
+          flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification!.title,
+            notification.body,
+            NotificationDetails(
+                android: AndroidNotificationDetails(
+                  channel.id,
+                  channel.name,
+                  icon: '@mipmap/ic_launcher',
+                ),
+                iOS: const DarwinNotificationDetails()),
+          );
+        }
+      }
+    });
+  } else {
+    debugPrint('User declined or has not accepted permission');
+  }
   await GlobalConfiguration().loadFromAsset('app_settings');
   await LocalStorage.init();
   runApp(const MyApp());
